2f1f624996c268830b9694482b9fca90c57ecf9a
willzhang <szgmwill@126.com>
2020-06-18 22:50:43 +0800

    init project

---
+++ NQueenSolution.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class NQueenSolution {
+   public static void main(String[] args) {
+       System.out.println("=======================");
+       String aaa = new String();
+       Set<Integer> set = new HashSet<Integer>();
+   }
+    
+}
